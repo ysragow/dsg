d46f6d959df68a3dfdd219bff91a94a501351125
John Sragow <ysragow@mit.edu>
2024-08-08 20:22:47 -0400

    adding scans and reducing processes

--- parquet/parallel.py
+++ parquet/parallel.py
@@ -5,6 +5,14 @@ from qd.qd_query import Query
 from qd.qd_table import Table
 
 
+def query_to_filters(query):
+    """
+    :param query: A QD query object
+    :return: That query transformed into a list of filters
+    """
+    return list(pred.to_dnf() for pred in query.list_preds())
+
+
 def table_concat(tables):
     """
     Concatenates a list of Arrow tables, which may have 0 or 1 elements, into a list of tables with 0 or 1 elements
@@ -29,29 +37,43 @@ def filter_read(items):
     return output
 
 
-def filter_queue(items, q, q2=None):
+def filter_scan(items):
     """
-    Reads a file using filters.  Adds the file to a queue.
-    :param q: A queue
-    :param q2: A second queue, used for debugging
+    Reads a file using filters.  This only exists because multiprocessing can't pickle non-global functions
     :param items: A list, containing a query object and a file name
+    :return: the result of running the query on that file
     """
     filters = items[0]
     file = items[1]
-    if q2 is not None:
-        q2.put('beginning to read ' + file)
     output = parquet.read_table(file, filters=filters)
-    if q2 is not None:
-        q2.put('finished reading ' + file)
-    q.put(output)
+    return output.shape[0]
 
 
-def parallel_read(query, files, processes, timestamps=False, verbose=False):
+def filter_queue(files, filters, q, q2=None, scan=False):
+    """
+    Reads a file using filters.  Adds the file to a queue.
+    :param files: a list of files to be read
+    :param filters: a list of filter
+    :param q: A queue
+    :param q2: A second queue, used for debugging
+    :param scan: Whether to scan instead of read
+    """
+    for file in files:
+        if q2 is not None:
+            q2.put('beginning to read ' + file)
+        output = parquet.read_table(file, filters=filters)
+        if q2 is not None:
+            q2.put('finished reading ' + file)
+        q.put(output.shape[0] if scan else output)
+
+
+def parallel_read(filters, files, processes, scan=False, timestamps=False, verbose=False):
     """
     Reads multiple files in parallel
-    :param query: A QD query object
+    :param filters: A list of filters
     :param files: A list of parquet file names
-    :param processes: A list of processes
+    :param processes: The number of processes running at the same time
+    :param scan: Whether to scan instead of reading
     :param timestamps: whether to return the total elapsed time
     :param verbose: whether to print things
     :return: A pyarrow table containing the contents of every file
@@ -61,29 +83,27 @@ def parallel_read(query, files, processes, timestamps=False, verbose=False):
     q2 = None
     if verbose:
         q2 = Queue()
-    filters = list(pred.to_dnf() for pred in query.list_preds())
     lists = [[filters, file] for file in files]
-    process_list = []
     if verbose:
         print("Generating processes...")
-    for i in range(len(lists)):
-        p = Process(target=filter_queue, args=(lists[i], q), kwargs={'q2': q2}, name=lists[i][1])
-        process_list.append(p)
-    if verbose:
-        print("Running processes...")
     active_processes = {}
     tables = []
+    sorted_files = [list() for _ in range(processes)]
 
-    # Start processes and add them to list of active processes
-    for process in process_list:
+    # Sort the files
+    for i in range(len(lists)):
+        sorted_files[i % processes].append(lists[i][1])
 
-        # Only add new ones when the list is not full
-        if verbose:
-            print("in the for loop")
-        while len(active_processes.keys()) == processes:
-            prune(active_processes)
-            flush(q, tables)
-            print_all(q2)
+        # # Only add new ones when the list is not full
+        # if verbose:
+        #     print("in the for loop")
+        # while len(active_processes.keys()) == processes:
+        #     prune(active_processes)
+        #     print_all(q2)
+
+    # Make and start processes and add them to list of active processes
+    for i in range(processes):
+        process = Process(target=filter_queue, args=(sorted_files[i], filters, q), kwargs={'q2': q2, 'scan': scan}, name=str(i))
 
         # Add processes to list of active processes
         process.start()
@@ -92,7 +112,7 @@ def parallel_read(query, files, processes, timestamps=False, verbose=False):
     # Wait for remaining processes to complete
     while active_processes:
         prune(active_processes)
-        flush(q, tables)
+        tables = flush(q, tables, scan=scan)
         print_all(q2)
 
     output = tables[0]
@@ -104,7 +124,7 @@ def parallel_read(query, files, processes, timestamps=False, verbose=False):
         print('\n')
         # print("Querying Time: ", end_query - start_time)
         print("Processes: ", processes)
-        print("Size: ", output.shape)
+        print("Size: ", output if scan else output.shape)
         print("Total Time: ", end_concat - start_time)
         print('\n')
         return end_concat - start_time
@@ -123,15 +143,16 @@ def prune(active_processes):
         del active_processes[key]
 
 
-def flush(q, tables):
+def flush(q, tables, scan=False):
     """
     Flush values from a queue to a list of tables
     :param q: A queue
     :param tables: A list of tables
+    :param scan: Whether to scan instead of reading
     """
     while not q.empty():
         tables.append(q.get())
-    table_concat(tables)
+    return [sum(tables)] if scan else table_concat(tables)
 
 
 def print_all(q):
@@ -144,49 +165,51 @@ def print_all(q):
             print(q.get())
 
 
-def pooled_read(query, files, processes, timestamps=False):
+def pooled_read(filters, files, processes, scan=False, timestamps=False):
     """
     Reads multiple files in parallel
-    :param query: A QD query object
+    :param filters: A list of filters
     :param files: A list of parquet file names
-    :param processes: A list of processes
+    :param processes: Number of  processes
+    :param scan: Whether to scan instead of read
     :param timestamps: whether to print timestamps of how long the query takes
     :return: A pyarrow table containing the contents of every file
     """
     start_time = time()
-    filters = list(pred.to_dnf() for pred in query.list_preds())
     items = [[filters, file] for file in files]
+    f = filter_scan if scan else filter_read
     with Pool(processes) as p:
-        output = p.map(filter_read, items)
+        output = p.map(f, items)
     end_query = time()
-    output = table_concat(output)[0]
+    output = sum(output) if scan else table_concat(output)[0]
     end_concat = time()
     if timestamps:
         print('\n')
         # print("Querying Time: ", end_query - start_time)
         print("Processes: ", processes)
-        print("Size: ", output.shape)
+        print("Size: ", output if scan else output.shape)
         print("Total Time: ", end_concat - start_time)
         print('\n')
         return end_concat - start_time
     return output
 
 
-def regular_read(query, files, timestamps=False):
+def regular_read(filters, files, scan=False, timestamps=False):
     """
     Sequentially read every provided file
-    :param query: A query
+    :param filters: A list of filters
     :param files: A list of files to read
+    :param scan: Whether to scan instead of read
     :param timestamps: whether to print timestamps for reading
     :return: All the data in the files, concatenated into one
     """
-    filters = list(pred.to_dnf() for pred in query.list_preds())
     output = []
     start_time = time()
     for file in files:
-        output.append(parquet.read_table(file, filters=filters))
-    output = table_concat(output)[0]
+        table = parquet.read_table(file, filters=filters)
+        output.append(table.shape[0] if scan else table)
+    output = sum(output) if scan else table_concat(output)[0]
     if timestamps:
-        print("Size: ", output.shape)
+        print("Size: ", output if scan else output.shape)
         print("Total Time: ", time() - start_time)
     return output
