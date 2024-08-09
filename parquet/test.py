210fe0885985c309df5dd471d6739e54095f994c
John Sragow <ysragow@mit.edu>
2024-08-08 20:36:56 -0400

    new testing file for parquet

--- parquet/test.py
+++ parquet/test.py
@@ -1,4 +1,4 @@
-from parallel import pooled_read, parallel_read, regular_read
+from parallel import pooled_read, parallel_read, regular_read, query_to_filters
 from parquet import generate_two_column, generate_query, get_matching_files
 
 s = 700
@@ -6,24 +6,36 @@ s = 700
 if __name__ == '__main__':
     name = 'test'
     print("Generating data...")
-    # generate_two_column(100000000, name, 900)
+    # generate_two_column(100000000, name, 90)
     print("Done")
-    query = generate_query(1/2, 99999999, 0)
+    query = generate_query(1/10, 99999999, 0)
     print(query)
     files = get_matching_files(query, name)
     print(files)
+    query = query_to_filters(query)
     print("Parallel Reads")
     v = False
-    # parallel_read(query, files, 10, timestamps=True, verbose=v)
-    # parallel_read(query, files, 5, timestamps=True, verbose=v)
-    # parallel_read(query, files, 2, timestamps=True, verbose=v)
-    # parallel_read(query, files, 1, timestamps=True, verbose=v)
+    parallel_read(query, files, 10, timestamps=True, verbose=v)
+    parallel_read(query, files, 5, timestamps=True, verbose=v)
+    parallel_read(query, files, 2, timestamps=True, verbose=v)
+    parallel_read(query, files, 1, timestamps=True, verbose=v)
+    print("Parallel Scans")
+    parallel_read(query, files, 10, timestamps=True, verbose=v, scan=True)
+    parallel_read(query, files, 5, timestamps=True, verbose=v, scan=True)
+    parallel_read(query, files, 2, timestamps=True, verbose=v, scan=True)
+    parallel_read(query, files, 1, timestamps=True, verbose=v, scan=True)
     print("Pooled Reads")
     pooled_read(query, files, 10, timestamps=True)
     pooled_read(query, files, 5, timestamps=True)
     pooled_read(query, files, 4, timestamps=True)
     pooled_read(query, files, 2, timestamps=True)
     pooled_read(query, files, 1, timestamps=True)
+    print("Pooled Scans")
+    pooled_read(query, files, 10, scan=True, timestamps=True)
+    pooled_read(query, files, 5, scan=True, timestamps=True)
+    pooled_read(query, files, 4, scan=True, timestamps=True)
+    pooled_read(query, files, 2, scan=True, timestamps=True)
+    pooled_read(query, files, 1, scan=True, timestamps=True)
     print("Regular Read")
     regular_read(query, files, timestamps=True)
     # name = 'test1'
