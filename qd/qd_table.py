4abf5886e40d61f9a4b51a280ad57f8f858c462e
John Sragow <ysragow@mit.edu>
2024-11-18 19:46:24 -0500

    trying to fix this stuff ONE MORE TIME (hopefully) (for real this time)

--- qd/qd_table.py
+++ qd/qd_table.py
@@ -199,7 +199,7 @@ def table_gen(path):
         c_np_type = file.dtypes[c_name]
         if str(c_np_type) == 'object':
             c_type = 'STRING'
-        elif str(c_np_type) in (Int32, Int64):
+        elif str(c_np_type) in ('Int32', 'Int64'):
             print("Column {} is interpreted as type {} in table {}".format(c_name, c_np_type, t_name))
             c_type = 'STRING'
         elif np.issubdtype(c_np_type, int):
