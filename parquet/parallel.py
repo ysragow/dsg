fae0bd8ce46a589f4bcfa8339dcdd4dba509706b
John Sragow <ysragow@mit.edu>
2025-01-19 17:05:04 -0500

    better print statements for pooled read, part 2

--- parquet/parallel.py
+++ parquet/parallel.py
@@ -239,7 +239,9 @@ def pooled_read(filters, files, processes, scan=False, timestamps=False, verbose
         num_rows = output if scan else output.shape[0]
         end_concat = time()
         total_size = sum([ParquetFile(file).count() for file in files])
-        print(f'Pooled Read on filters {filters} using {processes} processes scanned {num_rows} rows from files of size {total_size} in {end_concat - start_time} seconds')
+        total_time = end_concat - start_time
+        total_time = (total_time // 0.0000001) / 100000000
+        print(f'Filters {filters} using {processes} processes scanned {num_rows} out of {total_size} rows in {total_time} seconds.')
         if num_rows == 0:
             print(f"WARNING: 0 rows detected on query with filters {filters} scanning files {files}.")
     if timestamps:
