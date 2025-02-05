2826d559328abbca2e93b8576881b35cfc3b5b46
ysragow <ysragow@kite.upenn.edu>
2025-02-05 10:58:19 -0500

    sorting leaves, improving print statement further

--- pqd/join.py
+++ pqd/join.py
@@ -835,7 +835,8 @@ class PQD:
                     else:
                         file_index = (i + obj_num + 1) % split_factor
                     eff_dframes[file_index][obj] = df[df_index: min(df_index + chunk_size, size)]
-                    print(f"Making a file with {eff_dframes[file_index][obj].shape[0]} rows out of {size} from object {obj} and assigning it to {file_index}")
+                    if verbose:
+                        print(f"Making a chunk with {eff_dframes[file_index][obj].shape[0]} rows out of {size} from object {obj} and assigning it to {file_index}")
                     df_index += chunk_size
 
             # Create each file
